41fdd27fed17f327955e2015f0b9e60f02a7ba0a
GregShen <gregshen0925@gmail.com>
2022-07-13 16:56:10 +0800

    select action page

---
+++ src/Components/PageCreateWallet.js
@@ -0,0 +1,7 @@
+const bip39 = require('bip39')
+
+
+
+async function generate() {
+    const mnemonic = await bip39.generateMnemonic()
+}
---
+++ src/Components/PageSelectAction.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+function ImportWalletLayout() {
+  let navigate = useNavigate();
+  let path = `/import-wallet`;
+  navigate(path);
+}
+
+function CreateWalletLayout() {
+  let navigate = useNavigate();
+  let path = `create-wallet`;
+  navigate(path);
+}
+
+export default function PageB() {
+  return (
+    <div>
+      <button onClick={ImportWalletLayout}>Import wallet</button>
+      <button onClick={CreateWalletLayout}>Create a wallet</button>
+    </div>
+  )
+}
+
